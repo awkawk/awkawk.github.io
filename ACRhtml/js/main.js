8961ece9fb5f59aa7d4e6c5b4c3062f52362e6bc
Andrew Kirkpatrick <akirkpat@adobe.com>
2018-08-03 14:23:36 -0400

    Add files via upload

---
+++ ACRhtml/js/main.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+  console.log( "ready!" );
+  $.ajax({
+    type:"GET",
+    url:"js/test.json",
+    success: function(data) {
+      console.log(data);
+      var rowScript = $("#rowTemplate").html();
+      var mainInfoScript = $("#mainInfo").html();
+    
+      Handlebars.registerHelper('list', function(items, options) {
+        var out = "<tr>"; 
+        for(var i=0, l=items.length; i<l; i++) {
+          out = out + "<td>" + options.fn(items[i]) + "</td>";
+        }
+        return out + "</tr>";
+      });
+
+      // Compiling the templates
+      var rowTemplate = Handlebars.compile(rowScript);
+      var mainInfoTemplate = Handlebars.compile(mainInfoScript);
+
+      // Pass our data to the template
+      var rowCompiledHtml = rowTemplate(data);
+      var mainInfoCompiledHtml = mainInfoTemplate(data);
+      // Write your code here
+
+
+      // Add the compiled html to the page
+      $(".tableBody").html(rowCompiledHtml);
+      $(".basicInfo").html(mainInfoCompiledHtml);
+    },
+    dataType: 'json',
+  });
+});
